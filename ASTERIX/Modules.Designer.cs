55843e4fda7a76a4cddd9a9de4ba5f6ed9c744ee
Никита <@-19>
2018-05-27 16:47:15 +1000

    +Удаление модулей

--- ASTERIX/Modules.Designer.cs
+++ ASTERIX/Modules.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.ModulesGridView = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.AddModuleButton = new System.Windows.Forms.Button();
+            this.DeleteModuleButton = new System.Windows.Forms.Button();
+            this.ImportModuleDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.ModulesGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,56 +46,62 @@
             this.ModulesGridView.BackgroundColor = System.Drawing.SystemColors.Control;
             this.ModulesGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.ModulesGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.ModulesGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ModulesGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.ModulesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.ModulesGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ModulesGridView.DefaultCellStyle = dataGridViewCellStyle4;
             this.ModulesGridView.Location = new System.Drawing.Point(71, 12);
+            this.ModulesGridView.MultiSelect = false;
             this.ModulesGridView.Name = "ModulesGridView";
             this.ModulesGridView.ReadOnly = true;
             this.ModulesGridView.RowHeadersVisible = false;
             this.ModulesGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.ModulesGridView.Size = new System.Drawing.Size(479, 199);
             this.ModulesGridView.TabIndex = 0;
+            this.ModulesGridView.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.ModulesGridView_CellMouseDoubleClick);
+            this.ModulesGridView.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.ModulesGridView_UserDeletedRow);
+            this.ModulesGridView.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.ModulesGridView_UserDeletingRow);
             // 
-            // button1
+            // AddModuleButton
             // 
-            this.button1.Location = new System.Drawing.Point(123, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Добавить";
-            this.button1.UseVisualStyleBackColor = true;
+            this.AddModuleButton.Location = new System.Drawing.Point(123, 226);
+            this.AddModuleButton.Name = "AddModuleButton";
+            this.AddModuleButton.Size = new System.Drawing.Size(119, 40);
+            this.AddModuleButton.TabIndex = 1;
+            this.AddModuleButton.Text = "Добавить";
+            this.AddModuleButton.UseVisualStyleBackColor = true;
+            this.AddModuleButton.Click += new System.EventHandler(this.AddModuleButton_Click);
             // 
-            // button2
+            // DeleteModuleButton
             // 
-            this.button2.Location = new System.Drawing.Point(366, 226);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Удалить";
-            this.button2.UseVisualStyleBackColor = true;
+            this.DeleteModuleButton.Location = new System.Drawing.Point(366, 226);
+            this.DeleteModuleButton.Name = "DeleteModuleButton";
+            this.DeleteModuleButton.Size = new System.Drawing.Size(119, 40);
+            this.DeleteModuleButton.TabIndex = 2;
+            this.DeleteModuleButton.Text = "Удалить";
+            this.DeleteModuleButton.UseVisualStyleBackColor = true;
+            this.DeleteModuleButton.Click += new System.EventHandler(this.DeleteModuleButton_Click);
             // 
             // Modules
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(643, 344);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DeleteModuleButton);
+            this.Controls.Add(this.AddModuleButton);
             this.Controls.Add(this.ModulesGridView);
             this.Name = "Modules";
             this.Text = "Modules";
@@ -107,7 +114,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView ModulesGridView;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button AddModuleButton;
+        private System.Windows.Forms.Button DeleteModuleButton;
+        private System.Windows.Forms.OpenFileDialog ImportModuleDialog;
     }
 }
--- ASTERIX/Modules.cs
+++ ASTERIX/Modules.cs
@@ -17,6 +17,20 @@ namespace ASTERIX
         DataTable modules;
 
         /// <summary>
+        /// Возвращает байтовое представление файла.
+        /// </summary>
+        /// <param name="path">Путь к файлу.</param>
+        /// <returns>Байтовое представление файла</returns>
+        static byte[] LoadFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            byte[] buffer = new byte[fs.Length];
+            fs.Read(buffer, 0, Convert.ToInt32(fs.Length));
+            fs.Close();
+            return buffer;
+        }
+        
+        /// <summary>
         /// Сканирует папку Modules на наличие модулей.
         /// </summary>
         /// <returns>Таблица модулей. </returns>
@@ -35,9 +49,9 @@ namespace ASTERIX
 
             for (int path = 0; path < pathModules.Count; path++)
             {
-                Assembly asm = Assembly.LoadFile(Path.GetFullPath(pathModules[path]));
+                Assembly asm = Assembly.Load(LoadFile(Path.GetFullPath(pathModules[path])));
 
-                IList<CustomAttributeData> attr = asm.GetCustomAttributesData();
+                IList <CustomAttributeData> attr = asm.GetCustomAttributesData();
 
                 string DllType = attr.Where(x => x.Constructor.DeclaringType == typeof(AssemblyTitleAttribute)).First().ConstructorArguments.ElementAt(0).Value.ToString();
 
@@ -49,8 +63,11 @@ namespace ASTERIX
                     string Developer = attr.Where(x => x.Constructor.DeclaringType == typeof(AssemblyCompanyAttribute)).First().ConstructorArguments.ElementAt(0).ToString().Replace("\"", "");
 
                     DirectoryModules.Rows.Add(new object[] { false, Name, CAT, Version, Developer, asm });
-                    asm.GetType("Module").GetMethod("Init").Invoke(null, null);
+
+
+                    //asm.GetType("Module").GetMethod("Init").Invoke(null, null);
                 }
+
             }
             return DirectoryModules;
         }
@@ -120,7 +137,7 @@ namespace ASTERIX
                 
             foreach (DataRow module in on)
             {
-                XmlElement NewModuleNode = doc.CreateElement("rtept");
+                XmlElement NewModuleNode = doc.CreateElement("module");
                 NewModuleNode.IsEmpty = true;
                 NewModuleNode.SetAttribute("Name", module["Name"].ToString());
                 NewModuleNode.SetAttribute("CAT", module["CAT"].ToString());
@@ -133,6 +150,107 @@ namespace ASTERIX
             doc.Save("Settings.xml");
         }
 
+        /// <summary>
+        /// Добавляет новый модуль.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddModuleButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ImportModuleDialog.Filter = "DLL files (*.dll)|*.dll";
+                if (ImportModuleDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Assembly asm = Assembly.Load(LoadFile(ImportModuleDialog.FileName));
+
+                    IList<CustomAttributeData> attr = asm.GetCustomAttributesData();
+
+                    string DllType = attr.Where(x => x.Constructor.DeclaringType == typeof(AssemblyTitleAttribute)).First().ConstructorArguments.ElementAt(0).Value.ToString();
+
+                    if (DllType == "Module")
+                    {
+                        File.Copy(ImportModuleDialog.FileName, "Modules/" + ImportModuleDialog.SafeFileName);
+                        Modules_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранный файл не является модулем Flight Radar");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Удаляет файл модуля.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns>Результат операции.</returns>
+        bool DeleteModule(DataGridViewRow module)
+        {
+            if (MessageBox.Show("Удалить модуль?", "Подтверждение", MessageBoxButtons.YesNo,
+   MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                string ModuleFileName = module.Cells["Name"].Value.ToString() + ".dll";
+                File.Delete("Modules/" + ModuleFileName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Обработчик кнопки. Удаляет файл модуля.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteModuleButton_Click(object sender, EventArgs e)
+        {
+            if (DeleteModule(ModulesGridView.SelectedRows[0]))
+            {
+                ModulesGridView.Rows.Remove(ModulesGridView.SelectedRows[0]);
+                ModulesGridView.Refresh();
+                UpdateModules(modules);
+            }
+        }
+
+        /// <summary>
+        /// Изменяет состояние модуля.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ModulesGridView_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            modules.Rows[e.RowIndex]["Status"] = !(bool)modules.Rows[e.RowIndex]["Status"];
+            UpdateModules(modules);
+        }
+        /// <summary>
+        /// Удаляет выбранный модуль.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ModulesGridView_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            if (!DeleteModule(e.Row))
+            {
+                e.Cancel = true;
+            }
+        }
+        /// <summary>
+        /// Обновляет таблицу модулей.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ModulesGridView_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            UpdateModules(modules);
+        }
+
         public Modules()
         {
             InitializeComponent();
@@ -141,8 +259,7 @@ namespace ASTERIX
         private void Modules_Load(object sender, EventArgs e)
         {
             modules = GetModules();
-
-            //UpdateModules(modules);
+            UpdateModules(modules);
 
             ModulesGridView.DataSource = modules;
             ModulesGridView.Columns["Assembly"].Visible = false;
@@ -158,5 +275,6 @@ namespace ASTERIX
             ModulesGridView.Columns["Version"].SortMode = DataGridViewColumnSortMode.NotSortable;
             ModulesGridView.Columns["Developer"].SortMode = DataGridViewColumnSortMode.NotSortable; 
         }
+
     }
 }
